6361cd1d128053ea135b17c5b6d32926f3425c45
Ray Hutchinson <85248738+Hutch-Factor@users.noreply.github.com>
2025-07-29 16:26:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -23,18 +23,18 @@ assets_df = load_assets(path="data_assets.csv")
 st.write("CSV Column Names:", assets_df.columns.tolist())
 
 #Sidebar Filters
-#st.sidebar.header("🔍 Filter Assets")
-#status_filter = st.sidebar.multiselect("Status", options=assets_df["Status"].unique(), default=assets_df["Status"].unique())
-#location_filter = st. sidebar.multiselect("Location", options=assets_df["Location"].unique(), default=assets_df["Location"].unique())
+st.sidebar.header("🔍 Filter Assets")
+status_filter = st.sidebar.multiselect("status_update", options=assets_df["status_update"].unique(), default=assets_df["status_update"].unique())
+location_filter = st. sidebar.multiselect("Location", options=assets_df["Location"].unique(), default=assets_df["Location"].unique())
 
 #Filtered Data
-#filtered_df = assets_df[(assets_df["Status"].isin(status_filter)) & (assets_df["Location"].isin(location_filter))]
+#filtered_df = assets_df[(assets_df["status_update"].isin(status_filter)) & (assets_df["Location"].isin(location_filter))]
 
 #Display Summary Stats
 st.subheader("📊 Summary Statistics")
 col1, col2, col3 = st.columns(3)
 col1.metric("Total Assets", len(assets_df))
-col2.metric("In Repair", (assets_df["status"] == "In Repair").sum())
+col2.metric("In Repair", (assets_df["status_update"] == "In Repair").sum())
 col3.metric("Expired Warranty", (pd.to_datetime(assets_df["Warranty Expiry"]) < pd.to_datetime(date.today())).sum())
 
 #Asset Table
@@ -53,7 +53,7 @@ with st.form("new_asset_form"):
         serial = st.text_input("Serial Number")
         location = st.text_input("Location")
     with col2:
-        status = st.selectbox("Status", ["Active", "Operational", "In Repair", "Decommissioned"])
+        status_update = st.selectbox("status_update", ["Active", "In Repair", "Decommissioned"])
         acquisition_date = st.date_input("Acquisition Date")
         vendor = st.text_input("Vendor")
         warranty_expiry = st.date_input("Warranty Expiry")
@@ -66,7 +66,7 @@ with st.form("new_asset_form"):
             "Category": category,
             "Serial Number": serial,
             "Location": location,
-            "Status": status,
+            "Status Update": status_update,
             "Acquisition Date": acquisition_date,
             "Vendor": vendor,
             "Warranty Expiry": warranty_expiry
